34f0acf845560810d5dc2bc0b49fc41ed31ac63a
Grewi <grewi@ya.ru>
2025-05-16 12:22:24 +0400

    fix connect db for inst

--- inst/classes/connectDb.php
+++ inst/classes/connectDb.php
@@ -1,11 +1,10 @@
 <?php
 namespace system\inst\classes;
-// use system\core\database\database;
 use system\core\database\maryadb;
 use system\core\database\sqlite;
 use system\core\database\postgre;
 
-class connectDb //extends database
+class connectDb 
 {
 
     static public function c($configs)
@@ -17,15 +16,5 @@ class connectDb //extends database
             default => throw new \PDOException('Не указан подходящий тип базы данных')
         };
     }
-
-    // protected function __construct($configs)
-    // {
-    //     return match($configs['type']){
-    //         'mysql' => new maryadb($configs['host'], $configs['name'], $configs['user'], $configs['pass']),
-    //         'sqlite' => new sqlite(ROOT . '/sqlite/' . $configs['type'] . '.db'),
-    //         'postgre' => new postgre($configs['host'], $configs['name'], $configs['user'], $configs['pass']),
-    //         default => throw new \PDOException('Не указан подходящий тип базы данных')
-    //     };
-    // }
 
 }
