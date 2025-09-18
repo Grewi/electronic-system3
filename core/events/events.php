5c1c260de640232e5d685126fa8ae7700da3dee3
Grewi <grewi@ya.ru>
2025-09-18 10:38:33 +0400

    event

--- core/events/events.php
+++ core/events/events.php
@@ -1,35 +1,65 @@
-<?php 
+<?php
 
 namespace system\core\events;
 
+use system\core\logs\textLogs;
+
 abstract class events
 {
+    /**
+     * Список директорий внутри директории $this->dir по которым 
+     * будет осуществлятся поиск классов 
+     */
+    protected array  $list = [];
+    protected string $fileName;
+    protected bool   $logs = false;
+    protected string $dir = 'events';
+
+    /**
+     * @var fileName  имя файла/класса созданного в категории $this->dir и указанного в списке $this->list
+     */
+    public function __construct($fileName)
+    {
+        $this->fileName = $fileName;
+    }
+
+    /**
+     * Поиск файла в разделах и метода в них
+     */
+    protected function eventSearchClassAndMethod($name, $arguments)
+    {
+        foreach ($this->list as $el) {
+            $p = [$this->dir, $el, $this->fileName];
+            $c = implode('\\', $p);
+            $filePath =  implode('/', $p);
+            if (file_exists(ROOT . '/' . $filePath . '.php')) {
+                $class = new $c;
+                if (!method_exists($class, $name)) {
+                    $this->eventSearchLogErrors($c, $name);
+                    continue;
+                }
+                $class->{$name}(...$arguments);
+            } else {
+                $this->eventSearchLogErrors($c, $name);
+            }
+        }
+    }
+
+    protected function eventSearchLogErrors($class, $method = null)
+    {
+        if ($this->logs) {
+            $log = new textLogs(APP . '/cache/events', date('Y-m-d'));
+            if ($method) {
+                $log->error('class ' . $class . ' error method' . $method);
+            } else {
+                $log->error('error class ' . $class . ' method ' . $method);
+            }
+            $log->save();
+        }
+    }
+
     public function __call($name, $arguments)
     {
-        $this->search($name, $arguments);
+        $this->eventSearchClassAndMethod($name, $arguments);
     }
-
-    public static function __callStatic($name, $arguments)
-    {
-        (new static)->search($name, $arguments);
-    }
-
-    private function search($name, $arguments)
-    {
-        if (!property_exists($this, $name)) {
-            return false;
-        }
-        foreach ($this->{$name} as $i) {
-            if (!class_exists($i)) {
-                continue;
-            }
-            $class = new $i;
-            if(!method_exists($class, $name)){
-                continue;
-            }
-            $class->{$name}(...$arguments);
-        }
-    }
-
-    
 }
