a44c509307fba18702925e0dafbf391f9a9b24fa
grewi <grewi@ya.ru>
2025-12-25 10:57:44 +0400

    ++

--- core/events/text.php
+++ core/events/text.php
@@ -5,10 +5,17 @@ namespace system\core\events;
 trait text
 {
     private function strong($a, $b){
-        return 'с <strong>' . $a . '</strong> на <strong>' . $b . '</strong>';
+
+        return 'с <strong>' . $this->empty($a) . '</strong> на <strong>' . $this->empty($b) . '</strong>';
     }
 
     private function strongBool($a, $b){
         return 'с <strong>' . ($a == 1 ? 'Да':'Нет') . '</strong> на <strong>' . ($b == 1 ? 'Да' : 'Нет') . '</strong>';
+    }
+
+    private function empty(mixed $a)
+    {
+        $n = '<span style="color:#ccc; font-size:10px;"> (не указано) </span>';
+        return ($a === '' || is_null($a) ? $n : $a);      
     }
 }
